e4cc97e0f1451e676508fdf497d6a3ce0898a01a
alexhanl <alexhanl@qq.com>
2023-11-14 23:25:47 +0800

    update

--- bt_SimpleAIP.py
+++ bt_SimpleAIP.py
@@ -74,7 +74,6 @@ class SimpleAIPStrategy(bt.Strategy):   # Automatic investment plan (SIP) 基金
         yesterday = self.datas[0].datetime.date(ago=-1)
         
         if (today < yesterday) or (today.month != yesterday.month):  # 是否为第一个日期，或者每月第一个交易日，实际上交易是第二个交易日的净值
-            
             number_of_shares = self.cash_per_month / self.dataclose[0]
             self.log('BUY CREATE, Price = %.2f, Shares = %.2f' % (self.dataclose[0], number_of_shares))
             # self.order = self.buy(size=number_of_shares, exectype=bt.Order.Close)  # exectype=bt.Order.Close 以第二天的收盘价买入/卖出
@@ -85,21 +84,31 @@ class SimpleAIPStrategy(bt.Strategy):   # Automatic investment plan (SIP) 基金
 
 if __name__ == '__main__':
 
-    fund_data_df = prepare_cn_fund_data(FUND_HS300)
+    hs300_index_data_df = ak.stock_zh_index_daily_em(symbol="sh000300").iloc[:, :6]
+    hs300_index_data_df.columns = [
+    'date',
+    'open',
+    'close',
+    'high',
+    'low',
+    'volume',
+    ]
+    
+    # 把 date 作为日期索引，以符合 Backtrader 的要求
+    hs300_index_data_df.index = pd.to_datetime(hs300_index_data_df['date'])
 
     cerebro = bt.Cerebro()  # 初始化回测系统
-    start_date = datetime(2020, 1, 1)  # 回测开始时间
-    end_date = datetime(2020, 12, 31)  # 回测结束时间
-    data = bt.feeds.PandasData(dataname=fund_data_df, fromdate=start_date, todate=end_date)  # 加载数据
+    start_date = datetime(2022, 11, 1)  # 回测开始时间
+    end_date = datetime(2023, 11, 1)  # 回测结束时间
+    data = bt.feeds.PandasData(dataname=hs300_index_data_df, fromdate=start_date, todate=end_date)  # 加载数据
 
     cerebro.adddata(data)  # 将数据传入回测系统
     cerebro.addstrategy(SimpleAIPStrategy)  # 将交易策略加载到回测系统中
-    start_cash = 10000
+    start_cash = 240000
     cerebro.broker.setcash(start_cash)  # 设置初始资本为 100000
-    # cerebro.broker.setcommission(commission=0.0012)  # 设置交易手续费为 0.12%
-    cerebro.broker.set_coc(True) # 以订单创建日的收盘价成交 cheat-on-close
-    cerebro.broker.set_checksubmit(False) # 防止下单时现金不够被拒绝。只在执行时检查现金够不够。
-    # cerebro.broker.set_fundmode(fundmode=True)
+    cerebro.broker.setcommission(commission=0.00012)  # 设置交易手续费为 万分之 1.2
+    # cerebro.broker.set_coc(True) # 以订单创建日的收盘价成交 cheat-on-close
+    # cerebro.broker.set_checksubmit(False) # 防止下单时现金不够被拒绝。只在执行时检查现金够不够。
     
     cerebro.run()  # 运行回测系统
 
@@ -110,4 +119,7 @@ if __name__ == '__main__':
     print(f"总资金: {round(port_value, 2)}")
     print(f"净收益: {round(pnl, 2)}")
     
-    # cerebro.plot()
+    cerebro.plot()
+
+
+    # 结合PE
