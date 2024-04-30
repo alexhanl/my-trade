8a610c75f6194435ec2d2be2830f7d34ee1d9b85
alexhanl <alexhanl@qq.com>
2024-04-30 10:44:57 +0800

    更新了back_trade函数和相关的调用，优化了代码结构

---
+++ data_prepare_util.py
@@ -0,0 +1,108 @@
+import akshare as ak
+
+import os
+
+import pandas as pd
+
+DATA_PATH='./data'
+
+def download_fund_data(ticker: str):
+    """ 
+    利用 akshare 下载基金累计净值数据， 并且存放到data/<ticker>.csv文件中 
+    """
+    
+    data = ak.fund_open_fund_info_em(fund=ticker, indicator="累计净值走势")
+    data.columns = ['date', 'net_value']  # 重命名列名，以符合我们的习惯
+    filename = ticker + '.csv'
+    data.to_csv(os.path.join(DATA_PATH, filename), index=False)
+
+
+def load_fund_data(ticker, start_date, end_date) -> pd.DataFrame:
+    ''' 
+    从 csv 文件中读取加载基金累计净值数据，并且只截取 [start_date, end_date] 区间的数据
+    
+    Returns:
+    pd.DataFrame: index是date, 列包括'net_value'
+    '''
+    
+    data_df = pd.read_csv(os.path.join(DATA_PATH, ticker + '.csv'))
+    data_df['date'] = pd.to_datetime(data_df['date'])   
+    data_sliced_df = data_df[(data_df['date'] >= start_date) & (data_df['date'] <= end_date)]
+    data_sliced_df.set_index('date', inplace=True)
+    
+    return data_sliced_df
+
+
+def fill_non_trade_day_data(data_df, start_date, end_date) -> pd.DataFrame:
+    ''' 填补非交易日的数据 '''
+    # 如果 start_date 是非交易日, 则用第一个交易日的数据进行填补
+    if data_df['net_value'].get(start_date) == None:
+        first_trade_day_net_value = data_df['net_value'].iloc[0]
+        start_date_df = pd.DataFrame({'date': [start_date], 'net_value': [first_trade_day_net_value]})
+        start_date_df.set_index('date', inplace=True)
+        data_df = pd.concat([start_date_df, data_df])
+    
+    # 填补后面的非交易日
+    for i in range((end_date-start_date).days):
+        date = start_date + pd.DateOffset(i+1)        
+        net_value = data_df['net_value'].get(date)
+        if net_value == None:  # 非交易日，用上一个交易日的数据替代
+            last_day_net_value = data_df['net_value'].get(date - pd.DateOffset(1))
+            date_df = pd.DataFrame({'date': [date], 'net_value': [last_day_net_value]})
+            date_df.set_index('date', inplace=True)
+            data_df = pd.concat([data_df, date_df])
+    
+    data_df = data_df.sort_index()  
+    return data_df
+
+
+def load_portfolio_funds_data(ticker_series: pd.Series, start_date, end_date) -> pd.DataFrame:
+    """
+    获取portfolio 中每个基金的累计净值数据。
+
+    Parameters:
+    - ticker_series (pd.Series): 基金代码序列。
+    - start_date: 开始日期。
+    - end_date: 结束日期。
+
+    Returns:
+    - DataFrame: 各基金累计净值数据，日期为索引。
+    
+    date        163407  050025  000216  000402
+    2015-01-01  1.3066  1.4547  0.8980  1.0860
+    2015-01-02  1.3066  1.4547  0.8980  1.0860
+    ...            ...     ...     ...     ...
+    2023-10-28  2.1225  3.3480  1.6984  1.5129
+    """
+    
+    # 收集各基金的 DataFrame 列表
+    funds_data_frames = []
+    for ticker in ticker_series:
+        fund_data_df = fill_non_trade_day_data(load_fund_data(ticker, start_date, end_date), start_date, end_date)
+        fund_data_df = fund_data_df.rename(columns={'net_value': ticker})
+        funds_data_frames.append(fund_data_df)
+
+    # 使用 concat 一次性合并所有 DataFrame
+    if funds_data_frames:
+        portfolio_funds_data_df = pd.concat(funds_data_frames, axis=1)
+    else:
+        portfolio_funds_data_df = pd.DataFrame()
+
+    portfolio_funds_data_df = portfolio_funds_data_df.rename_axis('date')
+    return portfolio_funds_data_df
+
+if __name__ == "__main__":
+    
+    portfolio = [
+        ['000368', '汇添富沪深300安中指数A', 20],  
+        ['050025', '博时标普500', 20],
+        ['000216', '华安黄金ETF', 20],
+        ['400030', '东方天益', 8], 
+        ['000914', '中加纯债', 8],  
+        ['004388', '鹏华债券', 8], 
+        ['000032', '易方达信用债', 8],
+        ['000187', '华泰博瑞', 8]
+    ]
+
+    for item in portfolio:
+        download_fund_data(item[0])
--- fund_portfolio_back_trade.py
+++ fund_portfolio_back_trade.py
@@ -1,143 +1,18 @@
-import akshare as ak
 import matplotlib.pyplot as plt
 import pandas as pd
-import datetime as dt
-import os
+
+from data_prepare_util import load_portfolio_funds_data
+from data_calc_util import calculate_max_dd
 
 plt.rcParams["font.sans-serif"] = ["SimHei"]  # 设置字体
 plt.rcParams["axes.unicode_minus"] = False    # 该语句解决图像中的“-”负号的乱码问题
 
-NET_VALUE_DATE = '净值日期'
-ACCU_NET_VALUE = '累计净值'
 TOTAL_INVESTMENT = 100.0
-
 DATA_PATH='./data'
-
-def download_and_save_fund_data(ticker: str):
-    """ 利用 akshare 下载基金累计净值数据， 并且存放到data/<ticker>.csv文件中 """
-    data = ak.fund_open_fund_info_em(fund=ticker, indicator="累计净值走势")
-    filename = ticker + '.csv'
-    data.to_csv(os.path.join(DATA_PATH, filename), index=False)
-
-def load_fund_data(ticker, start_date, end_date) -> pd.DataFrame:
-    ''' 从 csv 文件中读取加载基金累计净值数据，并且只截取 [start_date, end_date] 区间的数据
-    
-    Returns:
-    pd.DataFrame: 一个单列的dataframe, index为时间, column为 '累计净值'
-    '''
-    data_df = pd.read_csv(os.path.join(DATA_PATH, ticker + '.csv'))
-    data_df[NET_VALUE_DATE] = pd.to_datetime(data_df[NET_VALUE_DATE])   
-    data_sliced_df = data_df[(data_df[NET_VALUE_DATE] >= start_date) & (data_df[NET_VALUE_DATE] <= end_date)]
-    data_sliced_df.set_index(NET_VALUE_DATE, inplace=True)
-    return data_sliced_df
-
-def fill_non_trade_day_data(data_df, start_date, end_date) -> pd.DataFrame:
-    ''' 填补非交易日的数据 '''
-    # 如果 start_date 是非交易日
-    if data_df[ACCU_NET_VALUE].get(start_date) == None:
-        first_trade_day_net_value = data_df[ACCU_NET_VALUE].iloc[0]
-        start_date_df = pd.DataFrame({NET_VALUE_DATE: [start_date], ACCU_NET_VALUE: [first_trade_day_net_value]})
-        start_date_df.set_index(NET_VALUE_DATE, inplace=True)
-        data_df = pd.concat([start_date_df, data_df])
-    
-    # 填补后面的非交易日
-    for i in range((end_date-start_date).days):
-        date = start_date + pd.DateOffset(i+1)        
-        net_value = data_df[ACCU_NET_VALUE].get(date)
-        if net_value == None:  # 非交易日，用上一个交易日的数据替代
-            last_day_net_value = data_df[ACCU_NET_VALUE].get(date - pd.DateOffset(1))
-            date_df = pd.DataFrame({NET_VALUE_DATE: [date], ACCU_NET_VALUE: [last_day_net_value]})
-            date_df.set_index(NET_VALUE_DATE, inplace=True)
-            data_df = pd.concat([data_df, date_df])
-    
-    data_df = data_df.sort_index()  
-    return data_df
-
-def load_potifolio_funds_data(ticker_series: pd.Series, start_date, end_date) -> pd.DataFrame:
-    """ 获取portfolio 中每个基金的累计净值数据
-    Parameters:
-    ticker_series (pd.Series): 参数1的描述
-    start_date
-    end_date
-
-    Returns:
-    DataFrame:  like below
-                163407  050025  000216  000402
-    date
-    2015-01-01  1.3066  1.4547  0.8980  1.0860
-    2015-01-02  1.3066  1.4547  0.8980  1.0860
-    2015-01-03  1.3066  1.4547  0.8980  1.0860
-    2015-01-04  1.3066  1.4547  0.8980  1.0860
-    2015-01-05  1.3066  1.4547  0.8980  1.0860
-    ...            ...     ...     ...     ...
-    2023-10-28  2.1225  3.3480  1.6984  1.5129
-    2023-10-29  2.1225  3.3480  1.6984  1.5129
-    2023-10-30  2.1256  3.3857  1.7028  1.5134
-    2023-10-31  2.1241  3.4059  1.7027  1.5141
-    2023-11-01  2.1242  3.4393  1.6976  1.5142
-    """
-    portfolio_funds_data_df = pd.DataFrame
-    for ticker in ticker_series:
-        fund_data_df = fill_non_trade_day_data(load_fund_data(ticker, start_date, end_date), start_date, end_date)  
-        fund_data_df = fund_data_df.rename(columns={ACCU_NET_VALUE: ticker})
-        
-        if (portfolio_funds_data_df.empty):
-            portfolio_funds_data_df = fund_data_df
-        else:
-            portfolio_funds_data_df = portfolio_funds_data_df.merge(fund_data_df, left_index=True, right_index=True)
-            
-    portfolio_funds_data_df = portfolio_funds_data_df.rename_axis('date')
-    return portfolio_funds_data_df
-
-
-def calculate_max_dd(value_series: pd.Series):  
-    ''' 计算最大回撤 '''
-    start_date = value_series.index[0]
-    end_date = value_series.index[-1]
-    
-    dd_peak_date_candidate = dd_peak_date = value_series.index[0]
-    dd_bottom_date_candidate = dd_bottom_date = dd_peak_date
-    dd_peak_value_candidate = dd_peak_value = value_series.iloc[0]
-    dd_bottom_value_candidate = dd_bottom_value = dd_peak_value
-    max_dd_candidate = max_dd = dd_peak_value - dd_bottom_value
-    
-    for i in range((end_date-start_date).days):
-        today = start_date + pd.DateOffset(i+1)
-        today_value = value_series[today]
-        
-        if (today_value > dd_peak_value):  # 一个下跌周期计算结束
-            
-            max_dd = dd_peak_value - dd_bottom_value
-            if (max_dd > max_dd_candidate): # 当前的回撤更高
-                dd_peak_date_candidate = dd_peak_date
-                dd_bottom_date_candidate = dd_bottom_date
-                dd_peak_value_candidate = dd_peak_value
-                dd_bottom_value_candidate = dd_bottom_value
-                max_dd_candidate = max_dd
-                
-            # 新的可能的下跌周期开始
-            dd_peak_date = dd_bottom_date = today
-            dd_peak_value = dd_bottom_value = today_value
-                    
-        elif (today_value < dd_bottom_value):  # 继续下跌
-            dd_bottom_date = today
-            dd_bottom_value = today_value
-    
-    # 计算最后一个周期
-    max_dd = dd_peak_value - dd_bottom_value      
-    if (max_dd > max_dd_candidate): # 当前的回撤更高 查看最后一个
-        dd_peak_date_candidate = dd_peak_date
-        dd_bottom_date_candidate = dd_bottom_date
-        dd_peak_value_candidate = dd_peak_value
-        dd_bottom_value_candidate = dd_bottom_value
-        max_dd_candidate = max_dd   
-    
-    return max_dd_candidate, dd_peak_date_candidate, dd_bottom_date_candidate, dd_peak_value_candidate, dd_bottom_value_candidate  
-        
 
 def back_trade(portfolio_df, start_date, end_date, rebalance_period, portfolio_funds_data_df):
     funds_value_dict = {}  # map of fund ticker to fund_value_df  对应与excel中，每个fund一个sheet，每个sheet记录 date，net_value（当天的基金单位净值），shares（持有份额），fund_value（基金持仓价值）
-    portfolio_total_value_series = pd.Series()  # portfolio_total_value_series 记录每天的portfolio的价值
+    portfolio_value_series = pd.Series()  # portfolio_total_value_series 记录每天的portfolio的价值
 
     # calculate the initial number of shares, fund value and total fund value
     start_date_net_value_series = portfolio_funds_data_df.iloc[0]
@@ -150,52 +25,45 @@ def back_trade(portfolio_df, start_date, end_date, rebalance_period, portfolio_f
                                                columns=['date', 'net_value', 'shares', 'fund_value'])
 
     # 计算start_date的初始 portfolio 整体 value，在没有佣金的情况下，应该为 TOTAL_INVESTMENT，也就是100
-    portfolio_value = 0.0 
-    for ticker in portfolio_df.index:
-        portfolio_value = portfolio_value + funds_value_dict[ticker]['fund_value'].iloc[0]
+    current_portfolio_value = sum(funds_value_dict[ticker]['fund_value'].iloc[0] for ticker in portfolio_df.index)
 
-    portfolio_total_value_series[start_date] = portfolio_value
+    portfolio_value_series[start_date] = current_portfolio_value
 
     # 计算每天的基金价值变化，从第二天开始
     day_counter = 0
     for date, funds_net_value in portfolio_funds_data_df.iloc[1:].iterrows():
         day_counter = day_counter + 1
-        portfolio_value = 0.0
+        current_portfolio_value = 0.0
         for ticker in portfolio_df.index:
             net_value = funds_net_value[ticker]
-            shares = funds_value_dict[ticker]['shares'].iloc[-1]  # 在没有交易的情况下，基金份额保持不变
+            shares = funds_value_dict[ticker]['shares'].iloc[-1]  # 在没有交易的情况下，基金份额保持不变，直接获取上一日的数据即可
             fund_value = net_value * shares
             funds_value_dict[ticker] = pd.concat([funds_value_dict[ticker], 
                                                  pd.DataFrame([[date, net_value, shares, fund_value]], 
                                                               columns = ['date', 'net_value', 'shares', 'fund_value'])], 
                                                 axis=0, ignore_index=True)
-            portfolio_value = portfolio_value + fund_value
+            current_portfolio_value = current_portfolio_value + fund_value
 
-        portfolio_total_value_series[date] = portfolio_value
+        portfolio_value_series[date] = current_portfolio_value
         
         # 重平衡，为了计算简单，直接在盘后即进行变更，并修正最后一行的数据（即当天的数据）
         if day_counter % rebalance_period == 0: 
-            print("---- 阶段业绩：  {} - {} ----".format( (date - pd.Timedelta(days=rebalance_period)).date(), date.date()))
+            # print("---- 阶段业绩：  {} - {} ----".format( (date - pd.Timedelta(days=rebalance_period)).date(), date.date()))
             for ticker in portfolio_df.index:
                 fund_value_df = funds_value_dict[ticker]
                 target_percent = portfolio_df.loc[ticker, 'target_percent']
-                fund_value_change = portfolio_value * target_percent/100 - fund_value_df['fund_value'].iloc[-1]  # 正数表示，需要加仓。负数相反
+                fund_value_change = current_portfolio_value * target_percent/100 - fund_value_df['fund_value'].iloc[-1]  # 正数表示未达到target_percent，需要加仓。负数相反
                 
-                fund_name = portfolio_df.loc[ticker, 'name']
-                profit = fund_value_df['fund_value'].iloc[-1] - fund_value_df['fund_value'].iloc[-rebalance_period]
-                print("{} 利润比例为 {:.2f} %".format(fund_name, profit/fund_value_df['fund_value'].iloc[-rebalance_period] * 100))
+                # fund_name = portfolio_df.loc[ticker, 'name']
+                # profit = fund_value_df['fund_value'].iloc[-1] - fund_value_df['fund_value'].iloc[-rebalance_period]
+                # print("{} 利润比例为 {:.2f} %".format(fund_name, profit/fund_value_df['fund_value'].iloc[-rebalance_period] * 100))
                 
                 shares_change =  fund_value_change / fund_value_df['net_value'].iloc[-1]  # 正数表示需要增加份额
                 fund_value_df.loc[fund_value_df.index[-1], 'shares'] += shares_change
-                fund_value_df.loc[fund_value_df.index[-1], 'fund_value'] = portfolio_value * target_percent/100
-            print('------')
-            
-    max_dd, dd_peak_date, dd_bottom_date, dd_peak_value, dd_bottom_value = calculate_max_dd(portfolio_total_value_series)
+                fund_value_df.loc[fund_value_df.index[-1], 'fund_value'] = current_portfolio_value * target_percent/100
     
-    return portfolio_total_value_series, max_dd, dd_peak_date, dd_bottom_date, dd_peak_value, dd_bottom_value
+    return portfolio_value_series
 
-
-    
 
 if __name__ == "__main__":
     
@@ -206,42 +74,23 @@ if __name__ == "__main__":
         ['400030', '东方天益', 8], 
         ['000914', '中加纯债', 8],  
         ['004388', '鹏华债券', 8], 
-        ['100018', '富国天利', 8],
+        ['000032', '易方达信用债', 8],
         ['000187', '华泰博瑞', 8]
-        ]
-    
-    # portfolio = [
-    #     # ['163407', '兴全沪深300', 20],
-    #     ['000368', '汇添富沪深300安中指数A', 20],
-    #     # ['000656', '前海开源沪深300指数A', 20],
-    #     # ['160807', '长盛沪深300指数', 20],
-    #     # ['002670', '万家沪深300指数增强A', 20],
-    #     # ['110020', '易方达沪深300ETF连接', 20],
-    #     ['050025', '博时标普500', 20],
-    #     ['000216', '华安黄金ETF', 20],
-    #     ['400030', '东方天益', 8],  # good
-    #     ['000914', '中加纯债', 8],  # good  
-    #     ['004388', '鹏华债券', 8],  # good
-    #     ['100018', '富国天利', 8],
-    #     ['530021', '建信纯债债券', 8],
-    #     # ['000187', '华泰博瑞', 8]
-    #     ]
+    ]
 
     portfolio_df = pd.DataFrame(portfolio, columns=['ticker', 'name', 'target_percent'])
     portfolio_df.set_index('ticker', inplace=True)
     
-    start_date = pd.to_datetime('2018-11-10')
-    end_date = pd.to_datetime('2023-11-10')
+    start_date = pd.to_datetime('2020-04-22')
+    end_date = pd.to_datetime('2024-04-22')
     
-    rebalance_period = 180#0000000 # 调仓间隔 days
-    
-    # download the data and save to csv files
-    for ticker in portfolio_df.index:
-        download_and_save_fund_data(ticker)
+    rebalance_days = 365  # 每 365 天 rebalance 一次
 
-    portfolio_funds_data_df = load_potifolio_funds_data(portfolio_df.index, start_date, end_date)
+    portfolio_funds_data_df = load_portfolio_funds_data(portfolio_df.index, start_date, end_date)
+    portfolio_value_series = back_trade(portfolio_df, start_date, end_date, rebalance_days, portfolio_funds_data_df)
     
-    portfolio_value_series, max_dd, dd_peak_date, dd_bottom_date, dd_peak_value, dd_bottom_value = back_trade(portfolio_df, start_date, end_date, rebalance_period, portfolio_funds_data_df)
+    # 计算相关 KPI，并展示结果
+    max_dd, dd_peak_date, dd_bottom_date, dd_peak_value, dd_bottom_value = calculate_max_dd(portfolio_value_series)
     max_dd_percent = max_dd/dd_peak_value*100
 
     portfolio_profit_percent = (portfolio_value_series.iloc[-1] - portfolio_value_series.iloc[0])/portfolio_value_series.iloc[0] * 100
@@ -250,7 +99,7 @@ if __name__ == "__main__":
 
     print("回测期间：{} - {}, 时长: {:.2f} 年".format(start_date.strftime('%Y-%m-%d'), end_date.strftime('%Y-%m-%d'), years))
     print("回测期间利润: {:.2f}%, 年化利润率： {:.2f}%".format(portfolio_profit_percent, portfolio_profit_annual))    
-    print("最大回撤开始日期: {}, 结束日期: {}".format(dd_peak_date.strftime('%Y-%m-%d'), dd_bottom_date.strftime('%Y-%m-%d')))
+    print("最大回撤开始日期: {}, 结束日期: {}, 持续 {} 天".format(dd_peak_date.strftime('%Y-%m-%d'), dd_bottom_date.strftime('%Y-%m-%d'), (dd_bottom_date-dd_peak_date).days))
     print("最大回撤: {:.2f} - {:.2f} = {:.2f}, 回撤比例: {:.2f}%".format(dd_peak_value, dd_bottom_value, max_dd, max_dd_percent))
     
     print("组合成员的情况：")
@@ -262,10 +111,8 @@ if __name__ == "__main__":
         max_dd, dd_peak_date, dd_bottom_date, dd_peak_value, dd_bottom_value = calculate_max_dd(portfolio_funds_data_df[ticker])
         max_dd_percent = max_dd/dd_peak_value*100
         print("{} {}: 利润 {:.2f}%, 年化：{:.2f}%, 最大回撤：{:.2f}%".format(ticker, portfolio_df.loc[ticker, 'name'], profit_percent, annaulized_profit_percent, max_dd_percent))
-    
-    
        
-    # 展示图
+    # 展示曲线图
     plt_data_df = portfolio_funds_data_df
     for ticker in plt_data_df.columns:
         plt_data_df = plt_data_df.rename(columns={ticker: ticker + portfolio_df.loc[ticker, 'name']})    
